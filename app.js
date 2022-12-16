660d7a746e209506e77eebdc387b16c56c2e973a
Vinith M H <vinithmh2000@gmail.com>
2022-12-16 23:51:01 +0530

    Port added

--- app.js
+++ app.js
@@ -38,6 +38,6 @@ app.post("/", function(req, res){
 
 
 
-app.listen(3000, function(req, res){
+app.listen(process.env.PORT || 3000, function(req, res){
   console.log("Server is running on port 3000.");
 })
